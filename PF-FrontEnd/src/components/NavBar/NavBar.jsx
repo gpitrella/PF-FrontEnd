ae327f97b3d83be544eb29bd924ea95027926604
gpitrella <gabriel98_@hotmail.com>
2022-06-21 11:40:45 -0300

    navbar actulizacion css martes 06

--- PF-FrontEnd/src/components/NavBar/NavBar.jsx
+++ PF-FrontEnd/src/components/NavBar/NavBar.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import searchIcon from './img/searchIcon.png';
-import imageLogo from './img/logo_tech_ecommerce.png';
+import imageLogo from './img/logoDotTech.gif';
 import imgCarrito from './img/img_carrito.png';
 import imgMensaje from './img/img_mensaje.png';
+import imgCorazon from './img/heart-icon.png'
 
 export default function NavBar() {
   const [toggleMenu, setToggleMenu] = useState(false)
@@ -37,13 +38,17 @@ export default function NavBar() {
        
     return (
         <nav>
+                <span className='navBarLogo'>
+                    <Link to="/"><img className="imageLogo" src={imageLogo} alt='Logo Tech Ecommerce' /></Link>
+                </span>
+                
+
+                
           {(toggleMenu || screenWidth > 500) && (
               
+            
             <ul className='list'>
-                <li className='navBarLogo'>
-                    <Link to="/"><img className="imageLogo" src={imageLogo} alt='Logo Tech Ecommerce' /></Link>
-                </li>
-                <li>
+                <li className="items">
                   <form>
                     <label className="switch">
                        Tema:
@@ -53,25 +58,32 @@ export default function NavBar() {
                   </form>
                 </li>
                 
-                <li>
+                <li className="items">
                   <form className="search-container">
-                    <input type='text' id='name' placeholder='Buscar Producto ...' autoComplete='off' onChange={(e) => handleSearch(e)}></input>
+
+                    
+                    <input type='text' className="inputSearchBar" id='name' placeholder='Buscar Producto ...' autoComplete='off' onChange={(e) => handleSearch(e)}></input>
                     <img type='submit' className="search-icon" src={searchIcon} alt='icono search' onClick={(e) => handleSubmit(e)}/>
                   </form>
+                </li >
+                <li className="items">
+                <Link to="/"><img className="imgCorazon" src={imgCorazon} alt='carrito de compra' /></Link>
                 </li>
-                <li>
+                
+                <li className="items">
                     <Link to="/"><img className="imgCarrito" src={imgCarrito} alt='carrito de compra' /></Link>
                 </li>
-                <li>
+                <li className="items">
                     <Link to="/"><img className="imgMensaje" src={imgMensaje} alt='casilla de mensajes' /></Link>
                 </li>
-                <li>
+                <li className="items">
                     <Link className="Login" to="/login">Login/Logout</Link>
                 </li>
 
                 </ul>
 
                 )}
+              
             
             <>
               <button className='btnNavbar' onClick={toggleNav}>BTN</button>
