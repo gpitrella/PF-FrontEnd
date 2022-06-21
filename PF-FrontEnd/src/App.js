31b3ee851675b79033d08800ffa8f2af1ec17762
cosm1co <federicofaraz@gmail.com>
2022-06-21 10:48:16 -0300

    footer

--- PF-FrontEnd/src/App.js
+++ PF-FrontEnd/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from './components/NavBar/NavBar';
 import ProductCard from './components/ProductCard/ProductCard';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           description = {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mattis.'}
           rating = {9.5}
        />
+        <Route path="/" component={Footer} />
       </Router>
     </React.Fragment>
   );
