af7c4497cbb9a5c8f4593163ad6789aee5c4fb9e
BeGamboa <bettinaigamboa@gmail.com>
2022-06-22 04:03:03 -0300

    Home component with ads

--- PF-FrontEnd/src/components/Home/Home.jsx
+++ PF-FrontEnd/src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '../../assets/logo_cyber_monday.png';
 import Brands from '../Brands/Brands';
 import Categories from '../Categories/Categories';
 import ProductCards from '../ProductCards/ProductCards';
@@ -9,7 +8,6 @@ export default function Home () {
     return (
         <div>
             <div className={style.ads}>
-                <img src={Logo} alt="cyber monday" />
             </div>
             <div className={style.catalogue}>
                 <h2>SECTION 1</h2>
