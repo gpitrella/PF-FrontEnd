f23ad0ab92259ac81fa829c6e714819984062f2d
BeGamboa <bettinaigamboa@gmail.com>
2022-07-02 12:24:44 -0300

    prueba

--- PF-FrontEnd/src/App.js
+++ PF-FrontEnd/src/App.js
@@ -24,6 +24,7 @@ import CheckOut from './components/CheckOut/CheckOut';
 import { Redirect } from 'react-router-dom';
 import FAQs from './components/FAQs/FAQs';
 import Adresses from './components/Branches/Adresses';
+import CategPage from "./components/CategTable/CategPage/CategPage";
 
 function App() {
   const dispatch = useDispatch()
@@ -58,8 +59,8 @@ function App() {
               <Route exact path="/store/brand/:brand" component = {Store} />
               <Route exact path="/productdetails/:id" component={ProductDetails} />
               <Route exact path='/checkout'> {user?.user ? <CheckOut/> : <Redirect to="/login"/>}</Route>
-              {/*<Route exact path="/createproduct"> {user?.user?.admin ? <CreateProduct/> : <Redirect to="/"/>}</Route>
-              <Route exact path="/admin/categories"> {user?.user?.admin ? <CreateActivity/> : <Redirect to="/"/>}</Route>*/}
+              {/*<Route exact path="/createproduct"> {user?.user?.admin ? <CreateProduct/> : <Redirect to="/"/>}</Route>*/}
+              <Route exact path="/admin/categories" component = {CategPage} />
               
               <Route path = "/admin"> {user?.user?.admin ? <Admin/> : <Redirect to = "/"/>}</Route>
 {/*              <Route exact path="/admin/users/list"> {user?.user?.admin ? <List/> : <Redirect to="/"/>}</Route>
