9cd0e78f099a068251232e88e9898379b524611b
Federico <romerof14@gmail.com>
2022-07-02 18:37:50 -0300

    FEAT-SCROLLTOTOP-Cambios en el color.

--- PF-FrontEnd/src/components/NavBar/NavBar.jsx
+++ PF-FrontEnd/src/components/NavBar/NavBar.jsx
@@ -396,7 +396,9 @@ export default function NavBar() {
       {/* { displayUser ? {renderMenuProfileNormalUser} : {renderMenu} } */}
       {renderMenu}
     </Box>
-    <ScrollToTop smooth component={<ExpandLessIcon />} />
+    <div className = {'scrollToTop'}>
+      <ScrollToTop smooth component={<ExpandLessIcon />} />
+    </div>
       </nav>
     )
 };
