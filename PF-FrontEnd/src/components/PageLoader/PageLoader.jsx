4386975bec4f552957b363e8003b1fce106bd4a2
Federico <romerof14@gmail.com>
2022-07-02 13:19:57 -0300

    FEAT-PAGELOADER-Agregado diseño y progress bar al componente.

--- PF-FrontEnd/src/components/PageLoader/PageLoader.jsx
+++ PF-FrontEnd/src/components/PageLoader/PageLoader.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import Logo from '../SVG/Logo';
 
 import s from './PageLoader.module.css';
 
 export default function PageLoader({ setShow }) {
+
+  const [ widthFull ,setWidthFull ] = React.useState(false);
+
   React.useEffect(() => {
-    let idTimeOut = setTimeout(() => setShow(false), 1500);
+    let idTimeOut1 = setTimeout(() => setWidthFull(true), 100);
+    let idTimeOut2 = setTimeout(() => setShow(false), 1500);
     return (() => {
-      clearTimeout(idTimeOut);
+      clearTimeout(idTimeOut1);
+      clearTimeout(idTimeOut2);
     })
   }, []);
 
   return (
     <div className = {s.globalContainer}>
       <div className = {s.container}>
-        CARGANDO
+        <div className = {s.imageContainer}>
+          <div className = {s.svgContainer}>
+            <Logo />
+          </div>
+        </div>
+        <div className = {s.containerLoading}>
+          <div className = {`${s.loading} ${ widthFull ? s.fullLoading : '' }`}></div>
+        </div>
       </div>
     </div>
   );
