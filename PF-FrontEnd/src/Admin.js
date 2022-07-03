b06c0a29da2c3e86926fead51519d73dffe37f4d
Federico <romerof14@gmail.com>
2022-07-02 23:45:52 -0300

    FEAT-EditProduct-Se agrego componente para modificar mas a fondo un producto. Aun falta agregar el cambio de visibilidad y acoplarlo al dashboard.

--- PF-FrontEnd/src/Admin.js
+++ PF-FrontEnd/src/Admin.js
@@ -3,6 +3,7 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Home from "./pages/home/Home";
 import CreateProductPage from './pages/CreateProductPage/CreateProductPage'
+import EditProduct from './components/EditProduct/EditProduct';
 // import CreateActivity from './components/Categories/NewCategory';
 import List from './pages/list/List';
 import UserEdit from './pages/user/UserEdit'
@@ -28,7 +29,8 @@ const Admin = () => {
           <Route path = {`${url}/users/list`} component = {List}/>
           <Route path = {`${url}/user/edit/:id`} component = {UserEdit}/>
           <Route path = {`${url}/products/list`} component = {ListProducts}/>
-          <Route path = {`${url}/products/createproduct`} component = {CreateProductPage}/>
+          <Route path = {`${url}/products/create`} component = {CreateProductPage}/>
+          <Route path = {`${url}/products/edit/:id`} component = {EditProduct}/>
           <Route path = '*' component = {notFoundPage} />
         </Switch>    
       </div>
