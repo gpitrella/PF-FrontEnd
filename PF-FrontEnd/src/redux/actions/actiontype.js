4ebb51bd4c8d5103ed870c29aa9316bd375369ac
Enzo <enz997.ing.ind@gmail.com>
2022-07-03 21:28:27 -0300

    .

--- PF-FrontEnd/src/redux/actions/actiontype.js
+++ PF-FrontEnd/src/redux/actions/actiontype.js
@@ -90,5 +90,5 @@ export const GET_LOCALIDADES = 'GET_LOCALIDADES';
 export const VALIDATING_ADDRESS = 'VALIDATING_ADDRESS';
 export const NORMALIZE_ADDRESS = 'NORMALIZE_ADDRESS';
 
-// BASE URL/
+// BASE URL//
 export const BASE_URL = 'https://techmarketapp.herokuapp.com';
